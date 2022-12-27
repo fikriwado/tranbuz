5d6e38e6ce5a511083ff9e64a5d27a83d97bcd4f
fikriwado <fixwad.online@gmail.com>
2022-12-28 06:31:36 +0700

    add models and migrations

---
+++ database/migrations/2022_12_27_231353_create_rute_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRuteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rute', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_pemberangkatan');
+            $table->unsignedBigInteger('id_pemberhentian');
+            $table->unsignedBigInteger('id_bus');
+            $table->time('jam_berangkat', $precision = 0);
+            $table->time('jam_sampai', $precision = 0);
+            $table->time('jam_transit', $precision = 0);
+            $table->foreign('id_pemberangkatan')->references('id')->on('lokasi')->onDelete('cascade');
+            $table->foreign('id_pemberhentian')->references('id')->on('lokasi')->onDelete('cascade');
+            $table->foreign('id_bus')->references('id')->on('bus')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rute');
+    }
+}
