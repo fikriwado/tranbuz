95f8f35058b42b2ceb75b3c18376d95363bff782
fikriwado <fixwad.online@gmail.com>
2022-12-29 09:07:27 +0700

    update writing welcome

--- resources/views/layouts/site/partials/header.blade.php
+++ resources/views/layouts/site/partials/header.blade.php
@@ -2,16 +2,16 @@
   <div class="container">
     <div class="row align-items-center position-relative">
       <div class="site-logo">
-        <a href="{{ url('/') }}" class="text-black"><span class="text-primary">Depot</a>
+        <a href="{{ url('/') }}" class="text-black"><span class="text-primary">{{ config('app.name', 'Tranbuz') }}</a>
       </div>
       <div class="col-12">
         <nav class="site-navigation text-right ml-auto " role="navigation">
           <ul class="site-menu main-menu js-clone-nav ml-auto d-none d-lg-block">
-            <li><a href="{{ Request::is('/') ? '#home-section' : url('/#home-section') }}" class="nav-link">Home</a></li>
-            <li><a href="{{ Request::is('/') ? '#services-section' : url('/#services-section') }}" class="nav-link">Services</a></li>
-            <li><a href="{{ Request::is('/') ? '#about-section' : url('/#about-section') }}" class="nav-link">About Us</a></li>
-            <li><a href="{{ Request::is('/') ? '#testimonials-section' : url('/#testimonials-section') }}" class="nav-link">Testimonials</a></li>
-            <li><a href="{{ Request::is('/') ? '#contact-section' : url('/#contact-section') }}" class="nav-link">Contact</a></li>
+            <li><a href="{{ Request::is('/') ? '#home-section' : url('/#home-section') }}" class="nav-link">Beranda</a></li>
+            <li><a href="{{ Request::is('/') ? '#services-section' : url('/#services-section') }}" class="nav-link">Layanan</a></li>
+            <li><a href="{{ Request::is('/') ? '#about-section' : url('/#about-section') }}" class="nav-link">Tentang Kami</a></li>
+            <li><a href="{{ Request::is('/') ? '#testimonials-section' : url('/#testimonials-section') }}" class="nav-link">Testimoni</a></li>
+            <li><a href="{{ Request::is('/') ? '#contact-section' : url('/#contact-section') }}" class="nav-link">Kontak</a></li>
           </ul>
         </nav>
       </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,8 @@
     <div class="container">
       <div class="row align-items-center justify-content-center text-center">
         <div class="col-lg-12">
-          <h1>Transportations &amp; Logistics</h1>
-          <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dolorem eius eligendi esse quod?</p>
+          <h1>Transportasi Kota Tasikmalaya</h1>
+          <p class="mb-5">Digitalisasi transit transportasi umum bus di Terminal Indihiang Kota Tasikmalaya</p>
           <div class="row d-flex justify-content-center">
             <div class="col-lg-10">
               <form action="{{ url('search') }}" method="POST">
@@ -51,22 +51,22 @@
         <div class="service text-center">
           <a href="#"><img src="{{ asset('templates/site/images/fasilitas/1.png') }}" alt="Image" class="img-fluid"></a>
           <div class="px-md-3">
-            <h3><a href="#">Truck Insurance</a></h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae distinctio laudantium nulla eos numquam incidunt!</p>
+            <h3><a href="#">Kantin</a></h3>
+            <p>Penumpang telah di sediakan fasilitas kantin dengan harga terjangkau. Menjadi sarana tunggu bus dan penginstirahatan!</p>
           </div>
         </div>
         <div class="service text-center">
           <a href="#"><img src="{{ asset('templates/site/images/fasilitas/2.png') }}" alt="Image" class="img-fluid"></a>
           <div class="px-md-3">
-            <h3><a href="#">Plane Transportation</a></h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae distinctio laudantium nulla eos numquam incidunt!</p>
+            <h3><a href="#">Ruang Tunggu</a></h3>
+            <p>Fasilitas ruang tunggu khusus. Menjadi tempat penginstirahatan transit dan bisa menjadi sarana informasi!</p>
           </div>
         </div>
         <div class="service text-center">
           <a href="#"><img src="{{ asset('templates/site/images/fasilitas/3.png') }}" alt="Image" class="img-fluid"></a>
           <div class="px-md-3">
-            <h3><a href="#">Sea &amp; Ear Freight</a></h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae distinctio laudantium nulla eos numquam incidunt!</p>
+            <h3><a href="#">Transit</a></h3>
+            <p>Pemberhenetian bus transit dari dalam maupun luar kota. Bisa menjadi sarana istirahat dan lainnya!</p>
           </div>
         </div>
       </div>
@@ -80,8 +80,8 @@
     <div class="row mb-5 justify-content-center">
       <div class="col-md-7 text-center">
         <div class="block-heading-1">
-          <h2>Services</h2>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+          <h2>Layanan</h2>
+          <p>Kami menyediakan banyak layanan untuk penumpang dalam kota maupun luar kota demi kenyamanan perjalanan.</p>
         </div>
       </div>
     </div>
@@ -90,22 +90,22 @@
         <div class="icon mb-0">
           <span class="flaticon-warehouse"></span>
         </div>
-        <h3 class="mb-3">Warehouse</h3>
-        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. </p>
+        <h3 class="mb-3">Penitipan</h3>
+        <p>Ruang penitipan barang yang aman bagi penumpang.</p>
       </div>
       <div class="block__35630 text-center">
         <div class="icon mb-0">
           <span class="flaticon-airplane"></span>
         </div>
-        <h3 class="mb-3">Air Freight</h3>
-        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. </p>
+        <h3 class="mb-3">Gesit</h3>
+        <p>Layanan informasi tersedia dengan super cepat.</p>
       </div>
       <div class="block__35630 text-center">
         <div class="icon mb-0">
           <span class="flaticon-box"></span>
         </div>
-        <h3 class="mb-3">Package Forwarding</h3>
-        <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. </p>
+        <h3 class="mb-3">Porter</h3>
+        <p>Petugas porter yang siap membawakan barang bawaan.</p>
       </div>
     </div>
   </div>
@@ -124,128 +124,74 @@
   </div>
 </div>
 
-<div class="site-section bg-light" id="faq-section">
-  <div class="container">
-    <div class="row mb-5">
-      <div class="block-heading-1 col-12 text-center">
-        <h2>Frequently Ask Questions</h2>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-6">
-        <div class="mb-5" data-aos="fade-up" data-aos-delay="100">
-          <h3 class="text-black h4 mb-4">Possimus aut consequuntur incidunt?</h3>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-        </div>
-        <div class="mb-5" data-aos="fade-up" data-aos-delay="100">
-          <h3 class="text-black h4 mb-4">Dolorum esse odio quas architecto sint?</h3>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-        </div>
-        <div class="mb-5" data-aos="fade-up" data-aos-delay="100">
-          <h3 class="text-black h4 mb-4">Possimus aut consequuntur incidunt?</h3>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-        </div>
-      </div>
-
-      <div class="col-lg-6">
-        <div class="mb-5" data-aos="fade-up" data-aos-delay="100">
-          <h3 class="text-black h4 mb-4">Lorem ipsum dolor sit</h3>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-        </div>
-        <div class="mb-5" data-aos="fade-up" data-aos-delay="100">
-          <h3 class="text-black h4 mb-4">consequuntur incidunt?</h3>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-        </div>
-        <div class="mb-5" data-aos="fade-up" data-aos-delay="100">
-          <h3 class="text-black h4 mb-4">Possimus aut consequuntur incidunt?</h3>
-          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="block__73694 site-section border-top" id="why-us-section">
+<div class="block__73694 site-section bg-light border-top" id="why-us-section">
   <div class="container">
     <div class="row d-flex no-gutters align-items-stretch">
       <div class="col-12 col-lg-6 block__73422 order-lg-2" style="background-image: url('{{ asset('templates/site/images/depot_delivery_1.jpg') }}');" data-aos="fade-left" data-aos-delay="">
       </div>
       <div class="col-lg-5 mr-auto p-lg-5 mt-4 mt-lg-0 order-lg-1" data-aos="fade-right" data-aos-delay="">
-        <h2 class="mb-4 text-black">Why Depot</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam error aliquid, dolores animi obcaecati quisquam accusamus soluta?</p>
+        <h2 class="mb-4 text-black">Kenapa Pilih Kami?</h2>
+        <p>Terminal indhiang adalah terminal induk terbesar di kawasan Tasikmalaya. Menyediakan banyak layanan berguna untuk perjalanan.</p>
         <ul class="ul-check primary list-unstyled mt-5">
-          <li>Cargo express</li>
-          <li>Secure Services</li>
-          <li>Secure Warehouseing</li>
-          <li>Cost savings</li>
-          <li>Proven by great companies</li>
+          <li>Penitipan barang</li>
+          <li>Layanan cepat</li>
+          <li>Porter</li>
+          <li>Keamanan penumpang</li>
+          <li>Fasilitas lainnya</li>
         </ul>
       </div>
     </div>
   </div>
 </div>
 
-<div class="site-section bg-light block-13" id="testimonials-section" data-aos="fade">
+<div class="site-section block-13" id="testimonials-section" data-aos="fade">
   <div class="container">
     <div class="text-center mb-5">
       <div class="block-heading-1">
-        <h2>Happy Clients</h2>
+        <h2>Penumpang Senang</h2>
       </div>
     </div>
     <div class="owl-carousel nonloop-block-13">
       <div>
         <div class="block-testimony-1 text-center">
           <blockquote class="mb-4">
-            <p>&ldquo;The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt
-              and made herself on the way.&rdquo;</p>
+            <p>&ldquo;Lokasinya sangat strategis, nyaman dan bersih. Cukup banyak bus yang menuju ke luar kota, salah satunya Budiman, Sinar Jaya, Primajasa, Doa Ibu dan masih banyak bus yang masuk ke terminal ini, tinggal dipilih saja.&rdquo;</p>
           </blockquote>
 
           <figure>
             <img src="{{ asset('templates/site/images/person_1.jpg') }}" alt="Image" class="img-fluid rounded-circle mx-auto">
           </figure>
-          <h3 class="font-size-20 text-black">Ricky Fisher</h3>
+          <h3 class="font-size-20 text-black">Ujang Solihin</h3>
         </div>
       </div>
 
       <div>
         <div class="block-testimony-1 text-center">
           <blockquote class="mb-4">
-            <p>&ldquo;Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.&rdquo;</p>
+            <p>&ldquo;Terminal Bus Tasikmalaya luas sekali, tata letak nya sangat nyaman. Masjid terletak di bagian depan, jalur pintu masuk. Masjid nyaman, bersih.&rdquo;</p>
           </blockquote>
           <figure>
             <img src="{{ asset('templates/site/images/person_2.jpg') }}" alt="Image" class="img-fluid rounded-circle mx-auto">
           </figure>
-          <h3 class="font-size-20 mb-4 text-black">Ken Davis</h3>
+          <h3 class="font-size-20 mb-4 text-black">Faisal Akbar</h3>
         </div>
       </div>
 
       <div>
         <div class="block-testimony-1 text-center">
           <blockquote class="mb-4">
-            <p>&ldquo;A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.&rdquo;</p>
-          </blockquote>
-          <figure>
-            <img src="{{ asset('templates/site/images/person_1.jpg') }}" alt="Image" class="img-fluid rounded-circle mx-auto">
-          </figure>
-          <h3 class="font-size-20 text-black">Mellisa Griffin</h3>
-        </div>
-      </div>
-
-      <div>
-        <div class="block-testimony-1 text-center">
-          <blockquote class="mb-4">
-            <p>&ldquo;Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.&rdquo;</p>
+            <p>&ldquo; Lokasi berada di daerah Indihiang, sangat strategis dan terdapat kantin dan toilet umun di dalam terminal. Lokasi untuk menunggu pun nyaman.&rdquo;</p>
           </blockquote>
           <figure>
             <img src="{{ asset('templates/site/images/person_3.jpg') }}" alt="Image" class="img-fluid rounded-circle mx-auto">
           </figure>
-          <h3 class="font-size-20 mb-4 text-black">Robert Steward</h3>
+          <h3 class="font-size-20 text-black">Anindira Adelina</h3>
         </div>
       </div>
     </div>
   </div>
 </div>
-<div class="site-section" id="contact-section">
+<div class="site-section bg-light" id="contact-section">
   <div class="container">
     <div class="row">
       <div class="col-12 text-center mb-5" data-aos="fade-up" data-aos-delay="">
